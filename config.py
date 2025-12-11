bc48d6e54d68f431debc7c37066f936a22a8831f
mblystad <157058851+mblystad@users.noreply.github.com>
2025-12-11 11:16:11 +0100

    Add configuration for grid and colors in config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import pygame
+
+# Grid
+TILE_SIZE = 20
+GRID_WIDTH = 30
+GRID_HEIGHT = 20
+
+SCREEN_WIDTH = GRID_WIDTH * TILE_SIZE
+SCREEN_HEIGHT = GRID_HEIGHT * TILE_SIZE
+
+FPS = 10
+
+# Colors
+COLOR_BG = (10, 10, 10)
+COLOR_GRID = (40, 40, 40)
+COLOR_SNAKE = (0, 200, 0)
+COLOR_FOOD = (200, 30, 30)
+
+pygame.font.init()
+DEFAULT_FONT = pygame.font.SysFont("consolas", 20)
